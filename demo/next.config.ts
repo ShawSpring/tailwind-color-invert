7163ea67e6e74e990bb1418ddd226d8facdd8f33
ShawSpring <chunlaixiao@163.com>
2025-04-06 23:22:00 +0800

    fix: :rocket: next.config 配置静态资源路径，workflow里的自动加路径没用

--- demo/next.config.ts
+++ demo/next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output:"export",
+  output:"export", //将项目导出为静态HTML文件
+  // distDir:"out" //默认目录为 out
+  basePath:"/tailwind-color-invert", // github pages 上只能是子路径
+  assetPrefix:"/tailwind-color-invert", // 静态资源的访问前缀 
+  trailingSlash: false,    // 是否在URL末尾添加斜杠
 };
 
 export default nextConfig;
